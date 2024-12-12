f44cc9a89c6a9d0afaf197986d3178f18f1096ed
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-12-12 13:10:56 +0530

    first commit

---
+++ Module1/Day1/app.js
@@ -0,0 +1,13 @@
+//console.log("Shubham ")
+
+// console.log("first")
+
+
+// console.group("User info")
+// console.log("Name : Shubham")
+// console.log("Age : 43")
+// console.groupEnd()
+
+
+// alert("hello ji welcome to my website")
+prompt( )
