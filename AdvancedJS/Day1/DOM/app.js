1c7b64974e08657b962d34535d04f67ea1b5f05f
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-02-24 13:20:41 +0530

    Commit

---
+++ AdvancedJS/Day1/DOM/app.js
@@ -0,0 +1,32 @@
+// let headingTag = document.getElementById("heading")
+// console.log(headingTag)
+
+
+// let headingTag2 = document.getElementsByClassName("myheading")
+// console.log(headingTag2)
+
+
+// let allHeadingTags = document.getElementsByTagName("h1")
+// console.log(allHeadingTags)
+
+
+let boxUsingID = document.querySelector("#dabba")
+console.log(boxUsingID)
+
+let boxUsingCLass = document.querySelector(".box")
+console.log(boxUsingCLass)
+
+let boxUsingTagname = document.querySelector("div")
+console.log(boxUsingTagname)
+
+
+let allBoxes = document.querySelectorAll("div")
+console.log(allBoxes)
+
+
+let allBoxesUsingCLass = document.querySelectorAll(".box")
+console.log(allBoxesUsingCLass)
+
+
+let allBoxesUsingID = document.querySelectorAll("#dabba")
+console.log(allBoxesUsingID)
