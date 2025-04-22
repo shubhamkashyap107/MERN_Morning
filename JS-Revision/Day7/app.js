ba5520b446304871bfd57d1217a031d41ca95213
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-04-22 13:35:07 +0530

    Commit

--- JS-Revision/Day7/app.js
+++ JS-Revision/Day7/app.js
@@ -154,29 +154,312 @@
 
 
 
-let n = 4
+// let n = 4
 
 //* * * *
 //*     *
 //*     *
 //* * * *
 
+
+// for(let i = 1; i <= n; i++)
+// {
+//     let ans = ""
+
+//     for(let j = 1; j <= n; j++)
+//     {
+//         if(i == 1 || i == n || j == 1 || j == n)
+//         {
+//             ans += "* "
+//         }
+//         else
+//         {
+//             ans += "  "
+//         }
+//     }
+
+//     console.log(ans)
+// }
+
+
+
+
+// let n = 7
+
+
+// for(let i = 1; i <= n; i++)
+// {
+//     let ans = ""
+
+//     for(let j = 1; j <= i; j++)
+//     {
+//         // ans += "* "
+
+//         if(i == n || j == 1 || i == j)
+//         {
+//             ans += "* "
+//         }
+//         else
+//         {
+//             ans += "  "
+//         }
+
+//     }
+
+//     console.log(ans)
+// }
+
+
+
+
+// let n = 5
+
+// for(let i = 1; i <= n; i++)
+// {
+//     let ans = ""
+//     for(let j = 1; j <= n - i + 1; j++)
+//     {
+//         if(i == 1 || j == 1 || j == n - i + 1)
+//         {
+//             ans += "* "
+//         }
+//         else
+//         {
+//             ans += "  "
+//         }
+//     }
+//     console.log(ans)
+// }
+
+
+
+
+//        *
+//      * *
+//    *   *
+//  *     *
+//* * * * *
+
+// let n = 6
+
+// for(let i = 1; i <= n; i++)
+// {
+//     let ans = ""
+
+//     for(let j = 1; j <= n - i; j++)
+//     {
+//         ans += "  "
+//     }
+
+//     for(let k = 1; k <= i; k++)
+//     {
+//         // ans += "* "
+//         if(k == 1 || k == i || i == n)
+//         {
+//             ans += "* "
+//         }
+//         else
+//         {
+//             ans += "  "
+//         }
+        
+//     }
+
+//     console.log(ans)
+
+
+// }
+
+
+
+//*       * 
+//  *   * 
+//    *
+//  *  *
+//*      *
+
+
+// let n = 5
+
+// for(let i = 1; i <= n; i++)
+// {
+//     let ans = ""
+//     for(let j = 1; j <= n; j++)
+//     {
+//         if(i == j || j == n - i + 1)
+//         {
+//             ans += "* "
+//         }
+//         else
+//         {
+//             ans += "  "
+//         }
+//     }
+//     console.log(ans)
+// }
+
+
+
+// let n = 4
+
+
+// for(let i = 1; i <= n; i++)
+// {
+//     let ans = ""
+
+//     for(let j = 1; j <= n - i; j++)
+//     {
+//         ans += "  "
+//     }
+
+//     for(let k = 1; k <= 2 * i - 1; k++)
+//     {
+//         ans += "* "
+//     }
+
+//     console.log(ans)
+
+// }
+
+// let n = 6
+// let sp = n - 1
+// let st = 1
+
+// for(let i = 1; i <= 2*n-1 ; i++)
+// {
+//     let ans = ""
+//     for(let j = 1; j <= sp; j++)
+//     {
+//         ans += "  "
+//     }
+//     for(let k = 1; k <= st; k++)
+//     {
+//         // ans += "* "
+//         if(k == 1 || k == st)
+//         {
+//             ans += "* "
+//         }
+//         else
+//         {
+//             ans += "  "
+//         }
+//     }
+
+//     console.log(ans)
+//     if(i < n)
+//     {
+//         sp--
+//         st+=2
+//     }
+//     else
+//     {
+//         sp++
+//         st-=2
+//     }
+// }
+
+
+
+// let n = 5
+// let stars = n
+// let spaces = 0
+
+// for(let i = 1; i <= 2 * n - 1; i++)
+// {
+//     let ans = ""
+
+//     for(let j = 1; j <= stars; j++)
+//     {
+//         ans += "* "
+//     }
+//     for(let k = 1; k <= spaces; k++)
+//     {
+//         ans += "  "
+//     }
+//     for(let j = 1; j <= stars; j++)
+//     {
+//         ans += "* "
+//     }
+
+//     console.log(ans)    
+    
+//     if(i < n)
+//     {
+//         stars--
+//         spaces+=2
+//     }
+//     else
+//     {
+//         stars++
+//         spaces-=2
+//     }
+// }
+
+
+
+
+// let n = 7
+// let stars = n
+// let spaces = 0
+
+// for(let i = 1; i <= 2 * n - 1; i++)
+// {
+//     let ans = ""
+
+//     for(let j = 1; j <= spaces; j++)
+//     {
+//         ans += " "
+//     }
+
+//     for(let j = 1; j <= stars; j++)
+//     {
+//         ans += "* "
+//     }
+
+
+//     console.log(ans)
+
+//     if(i < n)
+//     {
+//         spaces++
+//         stars--
+//     }
+//     else
+//     {
+//         spaces--
+//         stars++
+//     }
+// }
+
+
+
+let n = 7
+let stars = 1
+let spaces = Math.floor(n / 2)
 
 for(let i = 1; i <= n; i++)
 {
     let ans = ""
 
-    for(let j = 1; j <= n; j++)
+    for(let j = 1; j <= spaces; j++)
     {
-        if(i == 1 || i == n || j == 1 || j == n)
-        {
-            ans += "* "
-        }
-        else
-        {
-            ans += "  "
-        }
+        ans += "  "
     }
 
+    for(let j = 1; j <= stars; j++)
+    {
+        ans += "* "
+    }
     console.log(ans)
+    if(i < n / 2)
+    {
+        stars+=2
+        spaces--    
+    }
+    else
+    {
+        stars-=2
+        spaces++
+    }
+
 }
