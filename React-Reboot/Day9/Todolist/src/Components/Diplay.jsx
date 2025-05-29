e4c3f5b610140aa3aad58e90d05e2075e36119cc
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-05-29 13:08:17 +0530

    Commit

--- React-Reboot/Day9/Todolist/src/Components/Diplay.jsx
+++ React-Reboot/Day9/Todolist/src/Components/Diplay.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Diplay = () => {
+const Diplay = ({todos, setTodos, setItemToBeEdited}) => {
+  // console.log("Diplay Comp mounted")
+
+  // const[todos, setTodos] = useState([])
+
   return (
-     <div className='w-[50vw] border h-[100vh]'>Right</div>
+     <div className='w-[50vw] border h-[100vh] overflow-scroll'>
+          {todos && todos.map((item, index) => {
+            // console.log(item)
+            return (
+              <div key={index} className='p-2 mt-2 bg-blue-200 w-[80%] mx-auto flex justify-between items-center'>
+                
+                <div>
+                  <h2 className={'font-bold ' + (item.isCompleted ? "line-through" : "")} >{item.title}</h2>
+                  <p className={(item.isCompleted ? "line-through" : "")}>{item.desc}</p>
+                </div>
+
+              <div className='flex gap-2'>
+
+              <button onClick={() => {
+                let filteredTodos = todos.filter((i) => {
+                  return i.id != item.id
+                })
+                setTodos(filteredTodos)
+              }}>❌</button>
+
+
+              <button onClick={() => {
+                let newArray = todos.map((i) => {
+                  if(i.id == item.id)
+                  {
+                    item.isCompleted = !item.isCompleted
+                  }
+                  return i
+                })
+                setTodos(newArray)
+
+              }}>✅</button>
+              <button onClick={() => {
+                setItemToBeEdited(item)
+              }}>✏️</button>
+              </div>
+
+
+
+              </div>
+            )
+          })}
+     </div>
   )
 }
 
--- React-Reboot/Day9/Todolist/src/Components/Form.jsx
+++ React-Reboot/Day9/Todolist/src/Components/Form.jsx
@@ -1,15 +1,66 @@
 import { useState } from 'react'
+import { v4 as uuid } from 'uuid';
 
-const Form = () => {
+const Form = ({todos, setTodos, itemToBeEdited, setItemToBeEdited}) => {
+//   console.log("Form Comp mounted")
 
-    const[todos, setTodos] = useState([])
+    // const[todos, setTodos] = useState([])
+    const[title, setTitle] = useState("")
+    const[desc, setDesc] = useState("")
+
+
 
   return (
      <div className='w-[50vw] border h-[100vh]'>
-            <div className='border w-[50%] mx-auto p-2 mt-[30vh] flex flex-col gap-2 rounded-2xl border-gray-400 bg-yellow-100'>
-                <input className='bg-white border p-2 rounded-lg border-gray-400' type="text" placeholder='Enter task titile' />
-                <textarea className='bg-white border p-2 rounded-lg border-gray-400' name="" id="" placeholder='Enter task description'></textarea>
-                <button className='bg-green-300 w-[50%] mx-auto rounded-lg text-lg'>Add Task</button>
+            <div className='border w-[50%] mx-auto p-2 mt-[20vh] flex flex-col gap-2 rounded-2xl border-gray-400 bg-yellow-100'>
+                <input value={title} onChange={(e) => {
+                    setTitle(e.target.value)
+                }} className='bg-white border p-2 rounded-lg border-gray-400' type="text" placeholder='Enter task title' />
+                <textarea value={desc} onChange={(e) => {
+                    setDesc(e.target.value)
+                }} className='bg-white border p-2 rounded-lg border-gray-400' name="" id="" placeholder='Enter task description'></textarea>
+                <button onClick={() => {
+                    if(!title || !desc)
+                    {
+                        return
+                    }
+                    // const obj = {title, desc}
+                    // setTodos([...todos, obj])
+                    setTodos([...todos, {title, desc, id : uuid(), isCompleted : false}]) // setTodos(obj)
+                    setTitle("")
+                    setDesc("")
+                }} className='bg-green-300 w-[50%] mx-auto rounded-lg text-lg'>Add Task</button>
+            </div>
+
+
+            <div className='border w-[50%] mx-auto p-2 mt-10  flex flex-col gap-2 rounded-2xl border-gray-400 bg-yellow-100'>
+                <input value={itemToBeEdited.title} onChange={(e) => {
+                    // setTitle(e.target.value)
+                    setItemToBeEdited({...itemToBeEdited, title : e.target.value})
+                }} className='bg-white border p-2 rounded-lg border-gray-400' type="text" placeholder='Enter task title' />
+                <textarea value={itemToBeEdited.desc} onChange={(e) => {
+                    // setDesc(e.target.value)
+                    setItemToBeEdited({...itemToBeEdited, desc : e.target.value})
+                }} className='bg-white border p-2 rounded-lg border-gray-400' name="" id="" placeholder='Enter task description'></textarea>
+                <button onClick={() => {
+                    if(!itemToBeEdited.title || !itemToBeEdited.desc)
+                    {
+                        return
+                    }
+
+                    let newArr = todos.map((i) => {
+                        if(i.id == itemToBeEdited.id)
+                        {
+                            return itemToBeEdited
+                        }
+                        return i
+                    })
+
+                    setTodos(newArr)
+                    setItemToBeEdited({title : "", desc : ""})
+
+
+                }} className='bg-green-300 w-[50%] mx-auto rounded-lg text-lg'>Update Task</button>
             </div>
         </div>
   )
